7ca71dc8c8661926c31533ce8550a083640df0d6
Juho <ljlh9509@gmail.com>
2024-09-02 19:33:28 +0900

    first commit

---
+++ src/main/java/com/sajoproject/sajotuna/SajoTunaApplication.java
@@ -0,0 +1,13 @@
+package com.sajoproject.sajotuna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SajoTunaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SajoTunaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sajoproject/sajotuna/SajoTunaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sajoproject.sajotuna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SajoTunaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
