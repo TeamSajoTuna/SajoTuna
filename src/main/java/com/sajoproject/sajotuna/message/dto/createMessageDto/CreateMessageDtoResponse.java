5bf612b4f7e24163f9f82f54a5546dbbef330ef2
Juho <ljlh9509@gmail.com>
2024-09-04 23:22:56 +0900

    [FEAT] 쪽지 보내기 구현(이게 진짜임)

--- src/main/java/com/sajoproject/sajotuna/message/dto/createMessageDto/CreateMessageDtoResponse.java
+++ src/main/java/com/sajoproject/sajotuna/message/dto/createMessageDto/CreateMessageDtoResponse.java
@@ -1,2 +1,34 @@
-package com.sajoproject.sajotuna.message.dto.createMessageDto;public class CreateMessageDtoResponse {
+package com.sajoproject.sajotuna.message.dto.createMessageDto;
+
+import com.sajoproject.sajotuna.message.entity.Message;
+import com.sajoproject.sajotuna.user.entity.User;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class CreateMessageDtoResponse {
+    private Long messageId;
+
+    private String content;
+
+    private Long receiverId;
+
+    private Long senderId;
+
+    private LocalDateTime createdAt;
+
+    public CreateMessageDtoResponse(Message message){
+        this.messageId=message.getMessageId();
+        this.content=message.getContent();
+        this.receiverId=message.getReceiverId().getUserId();
+        this.senderId=message.getSenderId().getUserId();
+        this.createdAt=message.getCreatedAt();
+
+    }
+
 }
